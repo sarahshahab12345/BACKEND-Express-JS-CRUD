519117bbe3eb1f199a385e20c7c7984b55479e7d
Sarahsarahshahab12345 <sarahshahab2006@gmail.com>
2024-10-10 20:01:02 +0500

    Initial commit - Add Express.js CRUD application

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const port = process.env.PORT || 5001;
+
+import productRouter from "./routes/product-route.js";
+import connectToDb from "./config/db-config.js";
+connectToDb();
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/products", productRouter);
+
+app.listen(port, () => {
+  console.log(`server running on port http://localhost:${port}`);
+});
